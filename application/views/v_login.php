a81936ef795e9e94999bf7b8f7bbea356fa5216c
Muhammad Syahrevi <55977012+lizardhazard@users.noreply.github.com>
2020-10-16 14:20:03 +0700

    Add files via upload

---
+++ application/views/v_login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="<?php echo site_url('login/aksi_login/'); ?>" method="post">		
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Login"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
